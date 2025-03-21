9e60903c976c290c213c62d2f2d651ad6ad81e62
Dheeraj Sharma <dheeraj9410096874@gmail.com>
2025-03-21 17:27:18 +0530

    [Dheeraj Sharma] Add: UC2 implemented.

--- Email.js
+++ Email.js
@@ -1,5 +1,10 @@
 // Email.js
-
+// Function to validate the email format
+function isValidEmailFormat(email) {
+  // Regex to validate the mandatory parts "abc" and "bridgelabz"
+  const regex = /^abc.*@bridgelabz/;
+  return regex.test(email);
+}
 function validateEmail(email) {
   // Regex to validate the mandatory part "abc"
   const regex = /^abc/;
@@ -10,5 +15,5 @@ function validateEmail(email) {
 const email = "abc.xyz@bridgelabz.co.in";
 console.log(validateEmail(email)); // Output: true
 
-const invalidEmail = "xyz.abc@bridgelabz.co.in";
+const invalidEmail = "abc.xyz@bridgelabz.co.in";
 console.log(validateEmail(invalidEmail)); // Output: false
